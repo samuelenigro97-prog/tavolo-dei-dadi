b0db50b2411972e42854eca8cf6eb6ab9a9c988c
Samuele <samuele@Mac.fritz.box>
2026-07-16 16:19:04 +0200

    Bump version to 1.4.0

--- src/App.jsx
+++ src/App.jsx
@@ -1108,7 +1108,7 @@ const ESEMPIO_GNOMO = {
 
 const STORAGE_KEY = 'scheda-interattiva:v1';
 const STORAGE_KEY_LEGACY = 'tavolo-dei-dadi:scheda:v1';
-const APP_VERSION = '1.3.0';
+const APP_VERSION = '1.4.0';
 
 function nuovoId() {
   return 'pg-' + Date.now().toString(36) + Math.random().toString(36).slice(2, 6);
